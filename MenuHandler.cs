3ad7fa0f2bc06535739932406b24a3fa8177784c
dbeny <147709810+dbeny@users.noreply.github.com>
2024-05-10 09:07:50 +0200

    Update MenuHandler.cs

--- MenuHandler.cs
+++ MenuHandler.cs
@@ -7,23 +7,25 @@ namespace Aprojekt {
         private Menu currentMenu;
         private bool admin;
 
+        /*
+        A programnak egy darab MenuHandler-je van ami irányítja, hogy a felhasználó mikor, hogy és milyen menüket lát a képernyőn.
+        */
         public MenuHandler() {
             menus = new List<Menu>();
             admin = false;
         }
 
+        //beállítja, hogy a bejelentkezett felhasználó admin
         public void GiveAdmin() {
             admin = true;
         }
 
-        public void RevokeAdmin() {
-            admin = false;
-        }
-
+        //visszaadja, hogy megkapta-e az admin jogosultságot a MenuHandler
         public bool HasAdmin() {
             return admin;
         }
 
+        //átvált egy új Menu-re paraméterből majd meghívja a HandlePage függvényt
         public bool SwitchMenu(Menu newMenu) {
             if (!newMenu.GetId().Equals(currentMenu)) {
                 if (newMenu.GetId() == "admin" && !HasAdmin()) {
@@ -37,10 +39,19 @@ namespace Aprojekt {
             return false;
         } 
 
+        /*
+        irányítja, hogy a felhasználó léppen mit lát a képernyőn
+        a navigációt is itt kezeljük amivel a felh. 
+        vagy kilép a programból vagy egy másik menüre vált
+        */
         public void HandlePage() {
             ClearConsole();
             Console.WriteLine("----- EMPLOYEE MANAGER -----");
             Console.WriteLine();
+            /*
+            ha funkciós a menü (lásd: Menu.cs) akkor azt hívja meg 
+            és ha visszakap egy értéket akkor visszamegy a szülőmenübe
+            */
             if (currentMenu.IsFunctional()) {
                 bool returned = currentMenu.InvokeFunction();
                 if (returned) {
@@ -48,6 +59,7 @@ namespace Aprojekt {
                     return;
                 }
             }
+            //navigáció
             List<MenuOption> options = currentMenu.GetOptions();
             for (int i = 0; i < options.Count; i++) {
                 MenuOption opt = options[i];
@@ -61,6 +73,7 @@ namespace Aprojekt {
                     MenuOption opt = options[selected - 1];
                     string id = opt.Trigger();
                     Console.WriteLine("id: " + id);
+                    //ha a visszatérés menü lett meghívva akkor a szülőmenüre megy vissza
                     if (id == "back") {
                         SwitchMenu(currentMenu.GetParent());
                         return;
@@ -75,6 +88,7 @@ namespace Aprojekt {
             } else HandlePage();
         }
 
+        //menü hozzáadása a listába. ha ez az első akkor ki is mutatja
         public void AddMenu(Menu menu) {
             menus.Add(menu);
             if (menus.Count == 1) {
@@ -86,6 +100,7 @@ namespace Aprojekt {
             Console.Clear();
         }
 
+        //menü lekérése id alapján
         public Menu GetMenu(string id) {
             foreach (Menu m in menus) {
                 if (id.Equals(m.GetId())) return m;
